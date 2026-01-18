31431085f5220bf4d0fbf5639d5810b3e2b61054
Oumou-Toure <toureoumi203@gmail.com>
2026-01-18 14:40:19 +0100

    RED - test set_task et get_scheduled_tasks

---
+++ Tests/test_tdd_scheduler.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from scheduler import Scheduler
+from periodicites import EveryDayAt
+from heures import FakeClock
+
+def test_ajout_taches():
+    
+    heure = FakeClock(datetime(2026, 1, 18, 2, 0))
+    scheduler = Scheduler(heure)
+    scheduler.set_task("backup", EveryDayAt(3, 0), lambda: None)
+    
+    assert "backup" in scheduler.get_scheduled_tasks()
