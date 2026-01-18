ce083004542cf91a1883954df739131edfcf0250
Oumou-Toure <toureoumi203@gmail.com>
2026-01-18 21:00:22 +0100

    GREEN - ajout de la méthode remove_task pour la suppression d'une tache

--- scheduler.py
+++ scheduler.py
@@ -11,6 +11,10 @@ class Scheduler:
     def get_scheduled_tasks(self) -> List[str]:
         return list(self._tasks.keys())
     
+    def remove_task(self, name: str):
+        if name in self._tasks:
+            del self._tasks[name]
+
 
 class Task:
     def __init__(self, name: str, schedule, action: Callable):
